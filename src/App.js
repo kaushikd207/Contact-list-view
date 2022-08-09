25259b961ae94d43f4ae9b7111d23cd0598e1082
kaushikd207 <kaushikd207@gmail.com>
2022-08-10 02:19:52 +0530

    se9

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
         />
       )}
 
-      {contactList !== null && (
+      {(contactList !== null || contactList.length > 0) && (
         <>
           <h2>List of Contacts</h2>
           {contactList
