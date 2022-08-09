c44def0285897f93429c0c3af144bf047a121277
kaushikd207 <kaushikd207@gmail.com>
2022-08-10 00:58:11 +0530

    d

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -2,13 +2,13 @@ import '../card/App.css'
 export default function Card({ item, handleDelete, handleUpdate }) {
   return (
     <div className='card'>
-      <p className='name'><div>Name</div>{item.fullname}</p>
-      <p className='name'><div>Mail Id</div>{item.email}</p>
-      <p className='name'><div>Mobile</div>{item.phone}</p>
-      <p className='name'><div>Status</div>{item.status}</p>
+      <p className='name'><h4>Name</h4>{item.fullname}</p>
+      <p className='name'><h4>Mail Id</h4>{item.email}</p>
+      <p className='name'><h4>Mobile</h4>{item.phone}</p>
+      <p className='name'><h4>Status</h4>{item.status}</p>
       <div className='btnWrapper'>
         <button onClick={() => handleUpdate(item)}>Edit</button>
-        <button onClick={() => handleDelete(item.timestamp)}>delete</button>
+        <button onClick={() => handleDelete(item.timestamp)}>Delete</button>
       </div>
     </div>
   )
