81f28347a0c3a871a0ec25370f16ba7c0134703c
kaushikd207 <kaushikd207@gmail.com>
2022-08-12 01:13:58 +0530

    a

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,5 +1,5 @@
 import '../card/App.css'
-//To show the data in a card view
+//To show the data in a table view
 
 import { MdDelete, MdEdit } from 'react-icons/md'
 export default function Card({ item, handleDelete, handleUpdate }) {
@@ -20,33 +20,7 @@ export default function Card({ item, handleDelete, handleUpdate }) {
       </td>
     </tr>
 
-  // <div className="tableForm" id='customers'>
-  //     <table>
-  //       <thead>
-  //         <tr>
-  //           <th>First Name</th>
-  //           <th>Last Name</th>
-  //           <th>Email</th>
-  //           <th>Mobile </th>
-  //           <th>Status</th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         <tr>
-  //           <td>{item.FirstName}</td>
-  //           <td>{item.LastName}</td>
-  //           <td>{item.email}</td>
-  //           <td>{item.phone}</td>
-  //           <td>{item.status}</td>
-  //           <td>  <div className='btnWrapper'>
-  //       <button onClick={() => handleUpdate(item)}>Edit</button>
-  //       <button onClick={() => handleDelete(item.timestamp)}>Delete</button>
-  //     </div></td>
-  //         </tr>
-  //       </tbody>
 
-  //     </table>
-  //     </div>
  
   )
 }
